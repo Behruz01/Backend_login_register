b8709f137410acc13af616fee7ce5ce203284e0e
Behruz01 <ibragimovbehruz822@gmail.com>
2023-05-10 13:20:36 +0500

    change

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,5 +1,6 @@
 const Io = require("../utils/Io");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const Users = new Io("./db/users.json");
 exports.loginPost = async (req, res) => {
@@ -8,16 +9,22 @@ exports.loginPost = async (req, res) => {
     const { username, password } = body;
 
     const users = await Users.read();
-    const findUser = users.find(
-      (user) => user.username == username && user.password == password
-    );
+    const findUser = users.find((user) => user.username == username);
+
+    const deHash = await bcrypt.compare(password, findUser.password);
 
     if (!findUser) {
       res.status(403).json({ massage: "User not found" });
+      return;
+    }
+    if (!deHash) {
+      return res.status(403).json({ massage: "Incorrect password" });
     }
     // create token
     const secretKey = process.env.SECRET_CEY;
-    const token = jwt.sign({ id: findUser.id }, secretKey, { expiresIn: "1h" });
+    const token = jwt.sign({ id: findUser.id }, secretKey, {
+      expiresIn: "1h",
+    });
 
     res.status(200).json({ token: token });
   } catch (error) {
--- src/controllers/regis.controller.js
+++ src/controllers/regis.controller.js
@@ -1,6 +1,7 @@
 const Io = require("../utils/Io");
 const User = new Io("./db/users.json");
 const modelUser = require("../models/user");
+const bcrypt = require("bcrypt");
 
 exports.regisPost = async (req, res) => {
   try {
@@ -8,17 +9,18 @@ exports.regisPost = async (req, res) => {
     const { username, password } = body;
     const users = await User.read();
 
+    const hashPassword = await bcrypt.hash(password, 7);
+
     const findUser = users.find((user) => user.username == username);
     const id = (users[users.length - 1]?.id || 0) + 1;
-    const newUser = new modelUser(id, username, password);
-
+    const newUser = new modelUser(id, username, hashPassword);
     if (findUser) {
       res.status(403).json({ massage: "User already exists" });
+      return;
     }
     // write data
     const data = users.length ? [...users, newUser] : [newUser];
     User.write(data);
-
     res.status(200).json({ success: "Registreted" });
   } catch (error) {
     console.log(error);
